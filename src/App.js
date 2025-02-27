862d9f94473ee7218b420d8b89a952a4c4e4f544
RaJami1205 <rauljamiralfarorodriguez512@gmail.com>
2025-02-26 21:20:49 -0600

    Prueba 2 de commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bienvenido a React
+          Juego del Ahorcado
         </a>
       </header>
     </div>
